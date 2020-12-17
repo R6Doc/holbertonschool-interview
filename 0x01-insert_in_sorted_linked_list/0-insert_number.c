6ea580539a5ef21e5af86d66aa2b59ec27529927
r6doc <fiendgear@gmail.com>
2020-12-17 16:49:46 -0500

    add finished excersice

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -9,5 +9,44 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 
+listint_t *new_node, *value;
 
+if (head == NULL)
+    return (NULL);
+
+new_node = malloc(sizeof(listint_t));
+if (!new_node)
+    return (NULL);
+
+new_node->n = number;
+
+if (*head == NULL)
+{
+    *head = new_node;
+    new_node->next = NULL;
+    return (new_node);
+}
+
+if ((*head)->n > new_node->n)
+{
+    new_node->next = *head;
+    *head = new_node;
+    return (new_node);
+}
+
+if (*head != NULL)
+{
+    value = *head;
+while (value->next != NULL)
+{
+    if (value->next->n < new_node->n)
+        value = value->next;
+    else
+        break;
+}
+new_node->next = value->next;
+value->next = new_node;
+}
+
+return (new_node);
 }

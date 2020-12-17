6e34d1231ea43fa7b877fd2ab22195d26945ca7e
r6doc <fiendgear@gmail.com>
2020-12-16 20:01:41 -0500

    add nintial files

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * *insert_node - Function to insert node in singly linked list
+ * @head: First node of linked list
+ * @number: number of node
+ * Return: the address of the new node, or NULL if it failed
+ **/
+
+listint_t *insert_node(listint_t **head, int number)
+{
+
+
+}

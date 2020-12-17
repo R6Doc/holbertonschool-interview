d02b17b858c55a61dc488f07b0a0740048072716
r6doc <fiendgear@gmail.com>
2020-12-17 17:18:55 -0500

    fixed bety

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -12,41 +12,39 @@ listint_t *insert_node(listint_t **head, int number)
 listint_t *new_node, *value;
 
 if (head == NULL)
-    return (NULL);
-
+{
+return (NULL);
+}
 new_node = malloc(sizeof(listint_t));
 if (!new_node)
-    return (NULL);
-
+{
+return (NULL);
+}
 new_node->n = number;
-
 if (*head == NULL)
 {
-    *head = new_node;
-    new_node->next = NULL;
-    return (new_node);
+*head = new_node;
+new_node->next = NULL;
+return (new_node);
 }
-
 if ((*head)->n > new_node->n)
 {
-    new_node->next = *head;
-    *head = new_node;
-    return (new_node);
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
-
-if (*head != NULL)
-{
-    value = *head;
-while (value->next != NULL)
-{
-    if (value->next->n < new_node->n)
-        value = value->next;
-    else
-        break;
-}
+	if (*head != NULL)
+	{
+		value = *head;
+		while (value->next != NULL)
+		{
+			if (value->next->n < new_node->n)
+				value = value->next;
+			else
+				break;
+		}
 new_node->next = value->next;
 value->next = new_node;
 }
-
 return (new_node);
 }

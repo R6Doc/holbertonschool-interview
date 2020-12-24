3e268a36f7cefeb9bbd11ca598c6a6be9a7e571f
r6doc <fiendgear@gmail.com>
2020-12-23 21:38:28 -0500

    add all files

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * heap_insert -  function that creates a binary tree node
+ * @root: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * Return: pointer to the new node, or NULL on failure
+ */
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+
+
+}

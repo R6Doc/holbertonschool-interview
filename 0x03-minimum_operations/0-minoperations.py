a3598ffbf1522667e9a2956b829b97f0bf72a41a
r6doc <fiendgear@gmail.com>
2021-01-07 22:17:44 -0500

    fixed logic

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,13 +9,13 @@ def minOperations(n):
     if n <= 1:
         return 0
     count = 2
-    char_number = 2
-    copy = 1
+    chars = 2
+    temp = 1
 
-    while char_number < n:
-        if n % char_number == 0:
-            copy = char_number
+    while chars < n:
+        if n % chars == 0:
+            temp = chars
             count += 1
-        char_number += copy
+        chars += temp
         count += 1
     return count

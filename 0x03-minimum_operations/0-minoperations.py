38176e07030f1e172360e64d5fcc96daae14b046
r6doc <fiendgear@gmail.com>
2021-01-07 22:04:28 -0500

    finished files

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+""" Calculate fewest number of Operations"""
+
+
+def minOperations(n):
+    """ calculates number of operations needed for n H character """
+    if n <= 1:
+        return 0
+    count = 2
+    char_number = 2
+    copy = 1
+
+    while char_number < n:
+        if n % char_number == 0:
+            copy = char_number
+            count += 1
+        char_number += copy
+        count += 1
+    return count
